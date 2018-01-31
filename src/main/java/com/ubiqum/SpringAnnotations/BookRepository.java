233cdc5d117bcabcb8eb881408f1a8af3ebed44c
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-01-31 13:31:07 +0100

    Remove Spring Rest dependency

--- src/main/java/com/ubiqum/SpringAnnotations/BookRepository.java
+++ src/main/java/com/ubiqum/SpringAnnotations/BookRepository.java
@@ -1,11 +1,9 @@
 package com.ubiqum.SpringAnnotations;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource
 public interface BookRepository extends JpaRepository<Book, Long> {
 	List<Book> findByAuthor(String authorName);
 	List<Book> findByBookshelfLocation (String location);
--- src/main/java/com/ubiqum/SpringAnnotations/BookshelfRepository.java
+++ src/main/java/com/ubiqum/SpringAnnotations/BookshelfRepository.java
@@ -1,11 +1,9 @@
 package com.ubiqum.SpringAnnotations;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource
 public interface BookshelfRepository extends JpaRepository<Bookshelf, Long> {
 	List<Bookshelf> findByCapacityGreaterThan (Integer minimum);
 }
